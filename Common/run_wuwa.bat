8c065fba7c0634eeaf1ff468f0e99316e0c2bf2f
Brandy <26582143+AlteriaX@users.noreply.github.com>
2026-04-07 05:58:27 +0800

    Change info

--- Common/run_wuwa.bat
+++ Common/run_wuwa.bat
@@ -1,5 +1,6 @@
 @echo off
-:: Trim Engine.ini to only keep [SystemSettings] section before launching game
+:: Trim Engine.ini to only keep [SystemSettings], [Core.Log] and [/Script/Engine.RendererSettings] section before launching game
+:: Only r.RayTracing.LoadConfig is kept under [/Script/Engine.RendererSettings]
 :: File size will duplicate entries and get bigger if no trim
 powershell -NoProfile -Command "$lines = Get-Content 'Engine.ini'; $keep = $false; $renderer = $false; $out = @(); foreach ($line in $lines) { if ($line -ieq '[SystemSettings]' -or $line -ieq '[Core.Log]') { $keep = $true }; if ($line -ieq '[/Script/Engine.RendererSettings]') { $renderer = $true; $keep = $false; $out += $line; continue }; if ($keep) { if ($line -eq '') { $keep = $false; continue }; $out += $line } elseif ($renderer) { if ($line -ilike 'r.RayTracing.LoadConfig*') { $out += $line }; if ($line -eq '') { $renderer = $false } } }; $out | Set-Content 'Engine.ini'"
 
