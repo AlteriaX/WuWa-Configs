2385a07aa2906e4294167dd9317e95d43bbd7dff
Brandy <26582143+AlteriaX@users.noreply.github.com>
2026-04-07 05:51:55 +0800

    Update run_wuwa.bat

--- Common/run_wuwa.bat
+++ Common/run_wuwa.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Trim Engine.ini to only keep [SystemSettings] section before launching game
 :: File size will duplicate entries and get bigger if no trim
-powershell -NoProfile -Command "$lines=Get-Content 'Engine.ini'; $keep=$false; $out=@(); foreach ($line in $lines) { if ($line -ieq '[SystemSettings]') {$keep=$true}; if ($keep) { if ($line -eq '') {$keep=$false; continue}; $out+=$line }}; $out | Set-Content 'Engine.ini'"
+powershell -NoProfile -Command "$lines = Get-Content 'Engine.ini'; $keep = $false; $renderer = $false; $out = @(); foreach ($line in $lines) { if ($line -ieq '[SystemSettings]' -or $line -ieq '[Core.Log]') { $keep = $true }; if ($line -ieq '[/Script/Engine.RendererSettings]') { $renderer = $true; $keep = $false; $out += $line; continue }; if ($keep) { if ($line -eq '') { $keep = $false; continue }; $out += $line } elseif ($renderer) { if ($line -ilike 'r.RayTracing.LoadConfig*') { $out += $line }; if ($line -eq '') { $renderer = $false } } }; $out | Set-Content 'Engine.ini'"
 
 :: Start WuWa with launch options
 :: You can do this manually too by creating shortcut for Client-Win64-Shipping.exe and add the launch options
